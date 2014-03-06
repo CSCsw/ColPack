201e42b9c77b99c34458c4c93a4886e056774826
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-03-06 09:29:02 +0000

    address signed/unsigned comparison and size compatibility compiler warnings

--- SampleDrivers/Matrix_Compression_and_Recovery/SMB/sparse_jac_hess.cpp
+++ SampleDrivers/Matrix_Compression_and_Recovery/SMB/sparse_jac_hess.cpp
@@ -143,12 +143,12 @@ int main()
   //return 1;
 
   tapestats(tag_c,tape_stats);              // reading of tape statistics
-  printf("\n    independents   %d\n",tape_stats[0]);
-  printf("    dependents     %d\n",tape_stats[1]);
-  printf("    operations     %d\n",tape_stats[5]);
-  printf("    buffer size    %d\n",tape_stats[4]);
-  printf("    maxlive        %d\n",tape_stats[2]);
-  printf("    valstack size  %d\n\n",tape_stats[3]);
+  printf("\n    independents   %ld\n",(long)tape_stats[0]);
+  printf("    dependents     %ld\n",(long)tape_stats[1]);
+  printf("    operations     %ld\n",(long)tape_stats[5]);
+  printf("    buffer size    %ld\n",(long)tape_stats[4]);
+  printf("    maxlive        %ld\n",(long)tape_stats[2]);
+  printf("    valstack size  %ld\n\n",(long)tape_stats[3]);
 
 
 //------------------------------------------------------------------------------------
@@ -360,7 +360,8 @@ void printmatint(char* kette, int n, int m, int** M)
 
 
 void printmatint_c(char* kette, int m,unsigned int** M)
-{ int i,j;
+{ int i;
+  unsigned int j;
 
   printf("%s \n",kette);
   for (i=0;i<m;i++)
