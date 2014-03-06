7e906080c8fae85f1ea71394935de35b0217aa37
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2014-03-06 10:05:22 +0000

    compiler warnings: string literal char* vs const char*, and fscanf ret val

--- Utilities/mmio.cpp
+++ Utilities/mmio.cpp
@@ -74,7 +74,11 @@ int mm_read_unsymmetric_sparse(const char *fname, int *M_, int *N_, int *nz_,
  
     for (i=0; i<nz; i++)
     {
-        fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+        if (fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]) != 3)
+        {
+          fprintf(stderr, "error: mm read error\n");
+          return -1;
+        }
         I[i]--;  /* adjust from 1-based to 0-based */
         J[i]--;
     }
@@ -455,7 +459,7 @@ char *mm_strdup(const char *s)
 char  *mm_typecode_to_str(MM_typecode matcode)
 {
     char buffer[MM_MAX_LINE_LENGTH];
-    char *types[4];
+    const char *types[4];
 	char *mm_strdup(const char *);
     int error =0;
 
