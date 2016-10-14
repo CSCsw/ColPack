420034a529a037367c65a2c97ef2d55b6934c58a
probshin <cheng172@purdue.edu>
2016-10-14 14:17:30 -0400

    update

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -7,22 +7,45 @@ using namespace std;
 //Color: EXPLICIT_COVERING__STAR_BICOLORING, EXPLICIT_COVERING__MODIFIED_STAR_BICOLORING, IMPLICIT_COVERING__STAR_BICOLORING, IMPLICIT_COVERING__GREEDY_STAR_BICOLORING
 int main(int argc, const char* argv[]) {
     if(argc != 4) {
-        cerr<<"usage: "<<argv[0]<<" GraphName WayToOrder DISTANCE_ONE/TWO"<<endl;
+        cerr<<"usage: "<<argv[0]<<" GraphName"<<endl;
         cerr<<"Order: NATURAL, LARGEST_FIRST, DYNAMIC_LARGEST_FIRST, SMALLEST_LAST, INCIDENCE_DEGREE, RANDOM"<<endl;
         cerr<<"Distance: DISTANTCE_ONE, DISTANCE_TWO"<<endl;
         cerr<<"Color: EXPLICIT_COVERING__STAR_BICOLORING, EXPLICIT_COVERING__MODIFIED_STAR_BICOLORING, IMPLICIT_COVERING__STAR_BICOLORING, IMPLICIT_COVERING__GREEDY_STAR_BICOLORING"<<endl;
         return 0;
     }
-	string s_InputFile(argv[1]);
-	string s_order(argv[2]);
-	string s_distance(argv[3]);
 
-    GraphColoringInterface * g = new GraphColoringInterface(SRC_FILE, s_InputFile.c_str(), "AUTO_DETECTED");
-	g->Coloring(s_order.c_str(), s_distance.c_str());
-	cout << g->GetVertexColorCount()<<endl;
+    GraphColoringInterface * g = new GraphColoringInterface(SRC_FILE, argv[1], "AUTO_DETECTED");
+    cout<< "DISTANCE_ONE COLOR Result:" << endl;
+    g->Coloring("NATURAL", "DISTANCE_ONE");
+    cout<<"  NATURAL              :  " << g->GetVertexColorCount()<<endl;
+    g->Coloring("LARGEST_FIRST", "DISTANCE_ONE");
+    cout<<"  LARGEST_FIRST        : " << g->GetVertexColorCount()<<endl;
+    g->Coloring("DYNAMIC_LARGEST_FIRST", "DISTANCE_ONE");
+    cout<<"  DYNAMIC_LARGEST_FIRST: " << g->GetVertexColorCount()<<endl;
+    g->Coloring("SMALLEST_LAST", "DISTANCE_ONE");
+    cout<<"  SMALLEST_LAST        : " << g->GetVertexColorCount()<<endl;
+    g->Coloring("INCIDNECE_DEGREE", "DISTANCE_ONE");
+    cout<<"  INCIDENCE_DEGREE     : " << g->GetVertexColorCount()<<endl;
+    g->Coloring("RANDOM", "DISTANCE_ONE");
+    cout<<"  RANDOM               : " << g->GetVertexColorCount()<<endl;
 
-	delete g;
-	return 0;
+    cout<<endl;
+    cout<< "DISTANCE_TWO COLOR Result:" << endl;
+    g->Coloring("NATURAL", "DISTANCE_TWO");
+    cout<<"  NATURAL              :  " << g->GetVertexColorCount()<<endl;
+    g->Coloring("LARGEST_FIRST", "DISTANCE_ONE");
+    cout<<"  LARGEST_FIRST        : " << g->GetVertexColorCount()<<endl;
+    g->Coloring("DYNAMIC_LARGEST_FIRST", "DISTANCE_ONE");
+    cout<<"  DYNAMIC_LARGEST_FIRST: " << g->GetVertexColorCount()<<endl;
+    g->Coloring("SMALLEST_LAST", "DISTANCE_ONE");
+    cout<<"  SMALLEST_LAST        : " << g->GetVertexColorCount()<<endl;
+    g->Coloring("INCIDNECE_DEGREE", "DISTANCE_ONE");
+    cout<<"  INCIDENCE_DEGREE     : " << g->GetVertexColorCount()<<endl;
+    g->Coloring("RANDOM", "DISTANCE_ONE");
+    cout<<"  RANDOM               : " << g->GetVertexColorCount()<<endl;
+
+    delete g;
+    return 0;
 }
 
 
